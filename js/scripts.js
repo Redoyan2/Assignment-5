c8f5fd620244605a27fc89ce19112640cc40fd02
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-17 16:43:25 +0600

    calculation complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // get id function 
 
-function getElement(id){
+function getElement(id) {
     return document.getElementById(id);
 }
 
@@ -12,15 +12,41 @@ const saveInput = getElement('save-input');
 const totalBalance = getElement('total-balance');
 const totalExpense = getElement('total-expense');
 const savingAmount = getElement('saving-amount');
-const ramaingBalance = getElement('ramain-balance');
+const remaingBalance = getElement('ramain-balance');
 const erorrMasage = getElement('eror-masage');
 
-document.getElementById('calculate-btn').addEventListener('click', 
-function(){
-    const totalExpenseTaka = parseFloat(foodInput.value)+parseFloat(rentInput.value)+parseFloat(clothInput.value);
-    totalExpense.innerText = totalExpenseTaka;
-    totalBalance.innerText = parseFloat(incomeInput.value) - parseFloat(totalExpenseTaka);
+// expense and remaing balance calculation 
 
-    console.log(totalExpense.innerText);
 
-})
+
+document.getElementById('calculate-btn').addEventListener('click',
+    function () {
+        if (isNaN(incomeInput.value && foodInput.value && rentInput.value && clothInput.value && saveInput.value)) {
+            const totalExpenseTaka = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothInput.value);
+            totalExpense.innerText = totalExpenseTaka;
+            totalBalance.innerText = parseFloat(incomeInput.value) - parseFloat(totalExpenseTaka);
+
+            //  saving balance calculation 
+            document.getElementById('save-btn').addEventListener('click',
+                function () {
+                    const saveTaka = (parseFloat(incomeInput.value)) * ((parseFloat(saveInput.value)) / (100));
+                    savingAmount.innerText = parseFloat(saveTaka)
+                    remaingBalance.innerText = parseFloat(incomeInput.value) - (parseFloat(totalExpenseTaka) + parseFloat(saveTaka))
+                })
+            
+        }
+        else{
+            erorrMasage.innerText = 'Please enter a number!!!!'
+            incomeInput.value = '';
+            foodInput.value = '';
+            rentInput.value = '';
+            clothInput.value = '';
+            saveInput.value= '';
+            erorrMasage.style.display = 'block';
+        }
+
+
+    })
+
+
+
