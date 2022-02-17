64c2fcb5ab415a062aec5e910b90c149c3a66373
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-02-17 17:20:35 +0600

    error handle

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,17 @@ const erorrMasage = getElement('eror-masage');
 
 document.getElementById('calculate-btn').addEventListener('click',
     function () {
-        if (isNaN(incomeInput.value && foodInput.value && rentInput.value && clothInput.value && saveInput.value)) {
+        if (isNaN(incomeInput.value && foodInput.value && rentInput.value && clothInput.value) || (incomeInput.value < 0 || foodInput.value < 0 || rentInput.value < 0 || clothInput.value < 0)) {
+            erorrMasage.innerText = 'Please enter a positive number!!!!'
+            incomeInput.value = '';
+            foodInput.value = '';
+            rentInput.value = '';
+            clothInput.value = '';
+            saveInput.value = '';
+            erorrMasage.style.display = 'block';
+        }
+        else {
+
             const totalExpenseTaka = parseFloat(foodInput.value) + parseFloat(rentInput.value) + parseFloat(clothInput.value);
             totalExpense.innerText = totalExpenseTaka;
             totalBalance.innerText = parseFloat(incomeInput.value) - parseFloat(totalExpenseTaka);
@@ -29,20 +39,34 @@ document.getElementById('calculate-btn').addEventListener('click',
             //  saving balance calculation 
             document.getElementById('save-btn').addEventListener('click',
                 function () {
-                    const saveTaka = (parseFloat(incomeInput.value)) * ((parseFloat(saveInput.value)) / (100));
-                    savingAmount.innerText = parseFloat(saveTaka)
-                    remaingBalance.innerText = parseFloat(incomeInput.value) - (parseFloat(totalExpenseTaka) + parseFloat(saveTaka))
+                    if (isNaN(saveInput.value) || (saveInput.value<0)) {
+                        erorrMasage.innerText = 'Please enter a positive number!!!!';
+                        erorrMasage.style.display = 'block';
+                        saveInput.value = '';
+                    }
+                    else {
+                        const saveTaka = (parseFloat(incomeInput.value)) * ((parseFloat(saveInput.value)) / (100));
+
+                        const remainTaka = parseFloat(incomeInput.value) - (parseFloat(totalExpenseTaka) + parseFloat(saveTaka));
+                        if (remainTaka >= 0) {
+                            remaingBalance.innerText = remainTaka;
+                            savingAmount.innerText = parseFloat(saveTaka)
+                        }
+                        else {
+                            erorrMasage.innerText = 'Sufficient Balance!!!!!';
+                            erorrMasage.style.display = 'block';
+                            savingAmount.innerText = 'Suficient money!!!';
+                            remaingBalance.innerText = parseFloat(incomeInput.value) - parseFloat(totalExpenseTaka);
+                        }
+
+
+                    }
+
                 })
-            
-        }
-        else{
-            erorrMasage.innerText = 'Please enter a number!!!!'
-            incomeInput.value = '';
-            foodInput.value = '';
-            rentInput.value = '';
-            clothInput.value = '';
-            saveInput.value= '';
-            erorrMasage.style.display = 'block';
+
+
+
+
         }
 
 
